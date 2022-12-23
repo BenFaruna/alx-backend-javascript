30cf2975e5698245722ed8f7ef7bde4d3924c35c
BenFaruna <benjaminfaruna@gmail.com>
2022-12-23 15:37:05 +0100

    started 0x02-ES6_classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom";
+
+export default function initializeRooms() {
+  const roomStudents = [19, 20, 34];
+  const rooms = [];
+
+  for (const rs of roomStudents) {
+    rooms.push(new ClassRoom(rs));
+  }
+  return rooms;
+}
