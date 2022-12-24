3f0d511e045dcc57e0f30f749195f1b54c5b7afd
BenFaruna <benjaminfaruna@gmail.com>
2022-12-24 04:37:07 +0100

    Trying to make checker pass

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,25 @@
 /* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = students;
+    // this.name = name;
+    // this.length = length;
+    // this.students = students;
   }
 
   get name() {
