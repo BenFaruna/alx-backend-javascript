b2f15fb1bdf8843873e2d28a99f5be7fedcd2a06
BenFaruna <benjaminfaruna@gmail.com>
2022-12-24 05:35:55 +0100

    2-then.js completed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
-/* eslint no-unused-vars: ["error", { "args": "none" }] */
 export default function handleResponseFromAPI(promise) {
-  promise.then((success) => {
-    return {
-      status: 200,
-      body: 'success',
-    };
-  })
-    .catch(
-      (error) => { Error(); },
-    )
-    .finally(() => { 
-    console.log('Got a response from the API');
-  });
+  return promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+  .catch(() => { Error(); })
+  .finally(() => console.log('Got a response from the API'));
 }
