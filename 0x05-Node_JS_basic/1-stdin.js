256b969db35dd6766687c393964440239507d922
BenFaruna <benjaminfaruna@gmail.com>
2023-02-20 23:44:22 +0100

    1-stdin.js completed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,5 +9,5 @@ process.stdin.on('readable', () => {
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing' + '\n');
 });
