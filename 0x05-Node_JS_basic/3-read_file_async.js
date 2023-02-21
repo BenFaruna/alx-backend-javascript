5bf69e64571a1c7d08c1eb191f679fa1c289de02
BenFaruna <benjaminfaruna@gmail.com>
2023-02-21 01:39:00 +0100

    Completed 3-read_file_async

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+function extractStudentDetails(studentList) {
+  const students = {};
+  let count = 0;
+
+  for (let student of studentList) {
+    if (student !== '') {
+      student = student.trim();
+      student = student.split(',');
+      count += 1;
+
+      try {
+        students[student[student.length - 1]].push(student[0]);
+      } catch (error) {
+        students[student[student.length - 1]] = [student[0]];
+      }
+    }
+  }
+  return [count, students];
+}
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const result = [];
+
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      let log;
+      let database = data.toString();
+      database = database.split('\n').slice(1);
+      const [count, table] = extractStudentDetails(database);
+
+      log = `Number of students: ${count}`;
+      console.log(log);
+      result.push(log);
+
+      for (const field of Object.keys(table)) {
+        log = `Number of students in ${field}: ${table[field].length}. List: ${table[field].join(', ')}`;
+        console.log(log);
+        result.push(log);
+      }
+      resolve(result);
+    });
+  });
+}
+
+// countStudents('database.csv')
+
+module.exports = countStudents;
