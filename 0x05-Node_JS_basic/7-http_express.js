d7c158595d0a2f460ecc9ce6839af7efa2e1a615
BenFaruna <benjaminfaruna@gmail.com>
2023-02-21 23:45:53 +0100

    __

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const port = 1245;
-const hostname = '127.0.0.1';
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.end('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  res.setHeader('Content-Type', 'plain/text');
-  const path = process.execArgv.length > 0 ? process.execArgv[0] : 'database.csv';
-  const student = await countStudents(path);
-  res.write('This is the list of our students\n');
-  res.end(student.join('\n'));
+  try {
+    const students = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${students.join('\n')}`);
+  } catch (error) {
+    res.send(`This is the list of our students\n${error.message}`);
+  }
 });
 
-app.listen(port, hostname);
+app.listen(port, () => {});
 
 module.exports = app;
