2b487664cf0913d33934843add618fd394845416
BenFaruna <benjaminfaruna@gmail.com>
2023-02-21 01:03:40 +0100

    Completed 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,46 +1,42 @@
 const fs = require('fs');
 
 function extractStudentDetails(studentList) {
-  // const tableField = studentList[0];
-  const students = [];
+  const students = {};
+  let count = 0;
 
-  for (let student of studentList.slice(1)) {
+  for (let student of studentList) {
     if (student !== '') {
       student = student.trim();
-      students.push(student.split(','));
+      student = student.split(',');
+      count += 1;
+
+      try {
+        students[student[student.length - 1]].push(student[0]);
+      } catch (error) {
+        students[student[student.length - 1]] = [student[0]];
+      }
     }
   }
-  return students;
+  return [count, students];
 }
 
 function countStudents(path) {
   let database;
-  let csStudents = '';
-  let csStudentsCount = 0;
-  let sweStudents = '';
-  let sweStudentsCount = 0;
 
   try {
     database = fs.readFileSync(path).toString();
   } catch (error) {
     throw new Error('Cannot load the database');
   }
+  database = database.split('\n').slice(1);
 
-  const table = extractStudentDetails(database.split('\n'));
+  const [count, table] = extractStudentDetails(database);
 
-  for (const student of table) {
-    if (student[3] === 'CS') {
-      csStudentsCount += 1;
-      csStudents = csStudents !== '' ? `${csStudents}, ${student[0]}` : student[0];
-    } else {
-      sweStudentsCount += 1;
-      sweStudents = sweStudents !== '' ? `${sweStudents}, ${student[0]}` : student[0];
-    }
+  console.log('Number of students:', count);
+
+  for (const field of Object.keys(table)) {
+    console.log(`Number of students in ${field}: ${table[field].length}. List: ${table[field].join(', ')}`);
   }
-
-  console.log('Number of students:', table.length);
-  console.log(`Number of students in CS: ${csStudentsCount}. List: ${csStudents}`);
-  console.log(`Number of students in SWE: ${sweStudentsCount}. List: ${sweStudents}`);
 }
 
 countStudents('database.csv');
