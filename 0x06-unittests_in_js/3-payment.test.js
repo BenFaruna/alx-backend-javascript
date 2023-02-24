5d9d7628f4c4481fb6bffeff67f4ef1c4fadf0ae
BenFaruna <benjaminfaruna@gmail.com>
2023-02-24 01:06:04 +0100

    Using Sinon to create spy to track function calls and arguments

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const Utils = require('./utils');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('Checks to ensure calculateNUmber is used in sendPaymentRequestToApi', () => {
+  it('Checks calculateNumber is called once', () => {
+    sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+  });
+  it('Checks the arguments used to call the method', () => {
+    expect(Utils.calculateNumber.firstCall.args[1]).to.be.equal(100);
+    expect(Utils.calculateNumber.firstCall.args[2]).to.be.equal(20);
+  });
+  sinon.restore();
+});
